73cefdb84f9ea40dfdc8b2de8585d6844298f96e
ansman58 <javanslem@gmail.com>
2023-06-05 01:16:07 +0100

    rename file

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
 import Crew from "./pages/Crew";
 import Destination from "./pages/Destination";
 import Technology from "./pages/Technology";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import React from "react";
 
 function App() {
